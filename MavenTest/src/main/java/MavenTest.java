5764207285ce33434c8557dea86e59603e9e725f
Chris <fermat.little.theorem@gmail.com>
2015-12-06 23:08:19 +0800

    Add an example about maven.

---
+++ MavenTest/src/main/java/MavenTest.java
@@ -0,0 +1,10 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MavenTest {
+    static Log logger = LogFactory.getLog(MavenTest.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
