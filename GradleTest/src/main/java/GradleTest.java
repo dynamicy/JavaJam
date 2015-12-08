3f615568b97fbb4e16ad5e52e633d824f28f4604
Chris <fermat.little.theorem@gmail.com>
2015-12-08 23:22:45 +0800

    Add GradleTest.

---
+++ GradleTest/src/main/java/GradleTest.java
@@ -0,0 +1,11 @@
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class GradleTest {
+
+    static Log logger = LogFactory.getLog(GradleTest.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello World");
+    }
+}
